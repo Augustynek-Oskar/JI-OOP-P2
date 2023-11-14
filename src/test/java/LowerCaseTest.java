b9009c3ffbdedd875ddc89d2f885f89c03315fe3
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-14 23:29:27 +0100

    Added unit test for lowercase method.

---
+++ src/test/java/LowerCaseTest.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Assertions;
+
+public class LowerCaseTest extends TestCase {
+
+    public void testFormatText() {
+        String text = "Text";
+
+        LowerCase lowerCase = new LowerCase();
+        String result = lowerCase.formatText(text);
+        Assertions.assertEquals("text", result);
+    }
+}
