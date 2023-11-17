8d421a11de84bfd0796cd1f615220aaea09101fc
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-17 16:53:00 +0100

    added triangle constructor

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -8,23 +8,7 @@ public class Triangle {
         this.height = height;
     }
 
-    public String toString(){
+    public String toString() {
         return "Triangle {width= " + width + ", height= " + height + "}";
-    }
-
-public class Triangle extends Shape {
-    int sideA = 10;
-    int sideB = 5;
-    int sideC = 7;
-    int height = 5;
-
-    @Override
-    public float getArea() {
-        return (float) (7 * 5) / 2;
-    }
-
-    @Override
-    public float getPerimeter() {
-        return sideA + sideB + sideC;
     }
 }
